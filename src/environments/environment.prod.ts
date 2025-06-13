cf3bc46e77ba5a69feb665aa49d00d7cb66596f3
cfindlay-CT <130508465+cfindlay-CT@users.noreply.github.com>
2025-06-13 17:17:49 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,1 @@
-// src/environments/environment.ts
-export const environment = {
-  production: true,
-  serviceUrl: 'https://nodeapi-e9a4bwgncvbef6by.eastus2-01.azurewebsites.net/api/',
-  apiKey: ''
-};
+
