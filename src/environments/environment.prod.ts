b1d6223fb5e499263211f1ac96157ce2e2befe57
Clint <clintonf84@gmail.com>
2025-06-13 16:10:54 -0600

    fix url 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  serviceUrl: 'http://nodeapi-e9a4bwgncvbef6by.eastus2-01.azurewebsites.net/api/supabase',
+  serviceUrl: 'https://nodeapi-e9a4bwgncvbef6by.eastus2-01.azurewebsites.net/api/supabase',
   API_KEY: 'b4ffd001e10f67e9410a51110dbe1954f726c4c44ff8a883a80bddc470fd7880'
 };
