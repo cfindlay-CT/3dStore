1f63b9ac6ab0b4e242dea28e4b735726809e6619
Clint <clintonf84@gmail.com>
2025-06-13 22:20:54 -0600

    envir files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+// src/environments/environment.ts
+// This file can be replaced during build by using the `fileReplacements` array.
+// This file is generrated with GitHub Actions and contains the environment variables
+
