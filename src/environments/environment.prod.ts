f6f17c77032178dc779fa1508bc4562feab1196c
cfindlay-CT <130508465+cfindlay-CT@users.noreply.github.com>
2025-06-22 10:46:29 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-// src/environments/environment.ts
-// This file can be replaced during build by using the `fileReplacements` array.
-// This file is generrated with GitHub Actions and contains the environment variables
-
+export const environment = {
+  production: true,
+};
