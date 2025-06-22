0e41045c70f4679046f0d70d2fc92dd87ea61b75
Clint <clintonf84@gmail.com>
2025-06-22 10:43:53 -0600

    product update
    Made loading products faster

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-// src/environments/environment.ts
-// This file can be replaced during build by using the `fileReplacements` array.
-// This file is generrated with GitHub Actions and contains the environment variables
-
